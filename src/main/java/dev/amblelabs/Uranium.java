55c863db178377b0d3c20525e55add0551a8a3a1
loqor <loqor@amblelabs.dev>
2026-02-14 23:42:20 -0600

    init

---
+++ src/main/java/dev/amblelabs/Uranium.java
@@ -0,0 +1,22 @@
+package dev.amblelabs;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Uranium implements ModInitializer {
+	public static final String MOD_ID = "uranium";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static Identifier of(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("War. War never changes.");
+	}
+}
